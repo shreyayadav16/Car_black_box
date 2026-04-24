5927bd419f13d8087ef5850a7653dfde3b38754a
shreyayadav16 <152048097+shreyayadav16@users.noreply.github.com>
2026-04-24 20:04:06 +0530

    Add files via upload

---
+++ timer.h
@@ -0,0 +1,16 @@
+#ifndef TIMER_H
+#define	TIMER_H
+
+#define TIMER2_ENABLE            TMR2ON  
+#define TIMER2_PRESCALE0         T2CKPS0
+#define TIMER2_PRESCALE1         T2CKPS1
+#define TIMER2_PERIOD            PR2
+#define TIMER2_INTERRUPT_FLAG   TMR2IF
+#define TIMER2_INTERRUPT_ENABLE  TMR2IE
+
+void init_timer2(void);
+extern volatile unsigned char sec;
+#endif	/* TIMER_H */
+
+
+
